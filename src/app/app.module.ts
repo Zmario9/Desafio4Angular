308ba9bf6d76a4e19a5493284d004fd163f6a0c0
Jose Manuel <jcorreacastro99@gmail.com>
2023-04-20 14:13:31 -0400

    Primer commit, agregando el componente Header y asegurandome de que funcione io-icons, y que no haya incompatibilidades con ayuda de schemas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA //Agregando cosas ajenas a esta app
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
